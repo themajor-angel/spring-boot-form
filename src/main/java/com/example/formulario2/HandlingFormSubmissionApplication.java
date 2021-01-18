a485cc64f0354356bba41ef1a304162c3d9661ea
themajor-angel <majo.rangel97@gmail.com>
2021-01-18 10:55:22 -0500

    spring boot forms

---
+++ src/main/java/com/example/formulario2/HandlingFormSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.formulario2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlingFormSubmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandlingFormSubmissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/formulario2/Formulario2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formulario2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formulario2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
